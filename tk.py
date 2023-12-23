e71bdaee02e5f96e35b58dc5328a66b93f2974c4
jiangyin14 <1219439938@qq.com>
2023-12-23 20:39:51 +0800

    更新requirements并更新tk版

---
+++ tk.py
@@ -0,0 +1,52 @@
+import tkinter as tk
+import cv2
+from PIL import Image, ImageTk
+
+class VideoWallpaperApp:
+    def __init__(self, video_path, play_audio=True):
+        self.root = tk.Tk()
+        self.root.geometry("+0+0")  # 使窗口置于屏幕最顶层
+        self.root.overrideredirect(True)  # 隐藏标题栏和边框
+
+        self.video_path = video_path
+        self.play_audio = play_audio
+
+        self.canvas = tk.Canvas(self.root, width=self.root.winfo_screenwidth(), height=self.root.winfo_screenheight())
+        self.canvas.pack()
+
+        self.cap = cv2.VideoCapture(self.video_path)
+
+        # 获取视频帧率
+        self.fps = self.cap.get(cv2.CAP_PROP_FPS)
+        if self.fps == 0:
+            self.fps = 30  # 如果帧率为零，默认设置为30
+
+        self.show_frame()
+
+    def show_frame(self):
+        ret, frame = self.cap.read()
+        if ret:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            img = Image.fromarray(frame)
+            img = ImageTk.PhotoImage(img)
+            self.canvas.create_image(0, 0, anchor=tk.NW, image=img)
+            self.root.after(int(1000 / self.fps), self.show_frame)
+        else:
+            self.cap.release()
+            self.root.destroy()
+
+    def run(self):
+        self.root.mainloop()
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <video_path> [play_audio]")
+        sys.exit(1)
+
+    video_path = sys.argv[1]
+    play_audio = True if len(sys.argv) < 3 or int(sys.argv[2]) == 1 else False
+
+    app = VideoWallpaperApp(video_path, play_audio)
+    app.run()
